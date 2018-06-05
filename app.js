9c41405e108b0e6e1ea8fa0bc24fa3a8b6fac5a2
silviakazmierczyk <silvia.kazmierczyk@gmail.com>
2018-06-05 17:19:08 +0200

    p6

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
-var partials = require('express-partials');
-var flash = require('express-flash');
-var methodOverride = require('method-override');
+var logger = require('morgan');
 
-var index = require('./routes/index');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Configuracion de la session para almacenarla en BBDD usando Sequelize.
-var sequelize = require("./models");
-var sessionStore = new SequelizeStore({
-    db: sequelize,
-    table: "session",
-    checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds. (15 minutes)
-    expiration: 4 * 60 * 60 * 1000  // The maximum age (in milliseconds) of a valid session. (4 hours)
-});
-app.use(session({
-    secret: "Quiz 2018",
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(partials());
-app.use(flash());
 
-app.use('/', index);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
